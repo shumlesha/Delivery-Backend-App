dfa5c2831f46f29dca2969e537834ec6b4112a38
Aleksey Shumkov <shumleshas@gmail.com>
2023-10-12 10:12:50 +0700

    initial commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace webNET_Hits_backend_aspnet_project_1;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
