de1b2eb6f0ff0adef26e2d938df6ac21208af634
Aleksey Shumkov <shumleshas@gmail.com>
2023-10-14 19:07:32 +0700

    Updated db context.

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -6,6 +6,12 @@ public class AppDbContext: DbContext
 {
     public DbSet<User> Users { get; set; }
     public DbSet<Dish> Dishes { get; set; }
+    public DbSet<AddressElement> AddressElements { get; set; }
+    public DbSet<DishInCart> DishesInCart { get; set; }
+    public DbSet<Hierarchy> Hierarchies { get; set; }
+    public DbSet<House> Houses { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Rating> Ratings { get; set; }
     
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
