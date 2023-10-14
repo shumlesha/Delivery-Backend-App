2281acc55700989216042ee145043963af4b2b56
Aleksey Shumkov <shumleshas@gmail.com>
2023-10-14 18:06:14 +0700

    Created and added dto for models of the first necessity

---
+++ Models/DTO/OrderDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webNET_Hits_backend_aspnet_project_1.Models.DTO;
+
+public class OrderDTO
+{
+    public DateTime DeliveryTime { get; set; }
+    
+    [Required]
+    public DateTime OrderTime { get; set; }
+    
+    public Status Status { get; set; }
+    
+    public double Price { get; set; }
+    
+    public List<DishInCartDTO> Dishes  { get; set; }
+    
+}
